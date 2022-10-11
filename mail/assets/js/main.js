144a47a1e5bdf8f47b9ec9320c7ad1a60431d5f3
karfaisarra <karfaisarra@gmail.com>
2022-10-11 15:53:58 +0200

    add loop for and if

---
+++ mail/assets/js/main.js
@@ -0,0 +1,24 @@
+/* Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo */
+
+//creo una lista di email per che hanno l'accesso
+const mail = ['karfaisarra@gmail.com', 'aragosap@gmail.com', 'arnulfomarco7@gmail.com', 'barbierimattia.invest@gmail.com', 'pollon.fra@gmail.com'];
+//console.log(mail);
+
+const email = prompt('Inserisi la tua mail')
+//console.log(email);
+
+for(let i=0; i<mail.length; i++){
+    const element = mail[i];
+    //console.log(element);
+    if (email == element){
+        alert('Benvenuto!');
+    } else {
+        alert('Siamo spiacenti, non sei tra i partecipanti!');
+        break;
+    }
+}
+//Chiedi all’utente la sua email
+
+//controlla che sia nella lista di chi può accedere
+
+//stampa un messaggio appropriato sull’esito del controllo
