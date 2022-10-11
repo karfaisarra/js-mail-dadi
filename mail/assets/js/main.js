c751a6d5453986a5f75240bcb501cb8d717dc72a
karfaisarra <karfaisarra@gmail.com>
2022-10-11 15:56:20 +0200

    add comments

--- mail/assets/js/main.js
+++ mail/assets/js/main.js
@@ -4,12 +4,14 @@
 const mail = ['karfaisarra@gmail.com', 'aragosap@gmail.com', 'arnulfomarco7@gmail.com', 'barbierimattia.invest@gmail.com', 'pollon.fra@gmail.com'];
 //console.log(mail);
 
+//Chiedi all’utente la sua email
 const email = prompt('Inserisi la tua mail')
 //console.log(email);
 
 for(let i=0; i<mail.length; i++){
     const element = mail[i];
     //console.log(element);
+    //controlla che sia nella lista di chi può accedere+ stampa un messaggio appropriato sull’esito del controllo
     if (email == element){
         alert('Benvenuto!');
     } else {
@@ -17,8 +19,4 @@ for(let i=0; i<mail.length; i++){
         break;
     }
 }
-//Chiedi all’utente la sua email
 
-//controlla che sia nella lista di chi può accedere
-
-//stampa un messaggio appropriato sull’esito del controllo
